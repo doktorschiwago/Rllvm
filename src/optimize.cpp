6ca22da4db70863236175454432089bd2480a4d2
Chris <christoph.klein23@googlemail.com>
2017-10-23 21:01:45 +0200

    fix optimizeFunction

--- src/optimize.cpp
+++ src/optimize.cpp
@@ -71,21 +71,19 @@ extern "C"
 SEXP
 R_optimizeFunction(SEXP r_func, SEXP r_passMgr)
 {
-#if LLVM_VERSION == 3 && (LLVM_MINOR_VERSION >= 7 && LLVM_MINOR_VERSION <= 9)
+
+  llvm::Function *func = GET_REF(r_func, Function);
+#if (LLVM_VERSION == 3 && (LLVM_MINOR_VERSION >= 7 && LLVM_MINOR_VERSION <= 9)) || (LLVM_VERSION >= 4)
   llvm::legacy::FunctionPassManager *mgr = GET_REF(r_passMgr, legacy::FunctionPassManager);
+  mgr->run(*func);
 #else
   // This may not work for LLVM 4.0 or LLVM 3.( < 7)
   llvm::FunctionPassManager *mgr = GET_REF(r_passMgr, FunctionPassManager);
-#endif
-
-  llvm::Function *func = GET_REF(r_func, Function);
-
-#if LLVM_VERSION == 3 && LLVM_MINOR_VERSION < 10
-  mgr->run(*func);
-#else
   llvm::AnalysisManager<llvm::Function> AM;
   mgr->run(*func, AM);
 #endif
+
+
 
   return(Rf_ScalarLogical(TRUE));
 }
